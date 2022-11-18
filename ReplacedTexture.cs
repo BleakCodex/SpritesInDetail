18b627a02e699fa59c6ccd98f35b81556ce828a5
BleakCodex <BleakCodex@gmail.com>
2022-11-17 23:29:10 -0800

    Initial Commit

---
+++ ReplacedTexture.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace SpritesInDetail
+{
+    internal class ReplacedTexture : Texture2D
+    {
+        public Texture2D OriginalTexture {get; set;}
+        public Texture2D NewTexture { get; set; }
+
+        public HDTextureInfo HDTextureInfo {get; set;}
+        
+        public ReplacedTexture(Texture2D originalTexture, Texture2D newTexture, HDTextureInfo hdTextureInfo) : base(originalTexture.GraphicsDevice, newTexture.Width, newTexture.Height)
+        {
+            this.OriginalTexture = originalTexture;
+            this.NewTexture = newTexture;
+            this.HDTextureInfo = hdTextureInfo;
+        }
+    }
+}
